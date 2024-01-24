75150c84e6039419bab64094ff00a0c46f2915d0
YariMorcus <yarimorcus@zeelandnet.nl>
2024-01-24 17:14:01 +0100

    add helper function isEmpty

---
+++ src/js/helper.js
@@ -0,0 +1,7 @@
+/**
+ * Check if given string is empty
+ * @returns {boolean} true if empty, false otherwise
+ */
+export const isEmpty = (string) => {
+  return string.length === 0 ? true : false;
+};
