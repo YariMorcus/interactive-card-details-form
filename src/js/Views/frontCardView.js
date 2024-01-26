6c9cb81106dbed01762872265baf3176ec7ebc34
YariMorcus <yarimorcus@zeelandnet.nl>
2024-01-26 19:45:46 +0100

    return card number existing in 4 groups

--- src/js/Views/frontCardView.js
+++ src/js/Views/frontCardView.js
@@ -10,7 +10,7 @@ class FrontCardView extends View {
 
   renderCardNumber(number) {
     this._parentEl.querySelector('#js-card-front__card-number').innerText =
-      number;
+      this.#createNumberGroup(number);
   }
 
   renderExpDateMM(expDateMM) {
@@ -21,6 +21,20 @@ class FrontCardView extends View {
   renderExpDateYY(expDateYY) {
     this._parentEl.querySelector('#js-card-front__exp-date-yy').innerText =
       expDateYY;
+  }
+
+  /**
+   * Adds a whitespace after every 4th number to create a number existing of 4 separate groups
+   * @param {string} number single number string without groups (e.g. 1234567890123456)
+   * @returns {string} number group (e.g. 1234 5678 9012 3456)
+   */
+  #createNumberGroup(number) {
+    return [...number]
+      .map((num, ind) => {
+        return ind % 4 === 0 && ind !== 0 ? [' ', num] : num;
+      })
+      .flat()
+      .join('');
   }
 }
 
